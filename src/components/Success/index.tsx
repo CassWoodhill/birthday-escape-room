0eb8a095abe4fb685123e56d1eca3a4050213c81
Cass Woodhill <cwoodhill@kooth.com>
2024-03-13 16:42:33 +0000

    Update success message

--- src/components/Success/index.tsx
+++ src/components/Success/index.tsx
@@ -3,18 +3,9 @@ import css from "./Success.module.scss";
 export const Success: React.FC = () => {
   return (
     <div className={css.success}>
-      <h1>Shutdown Successful</h1>
+      <h1>Congratulations!</h1>
       <p>
-        You have succeeded in shutting down the GPT-42 system.
-        <br />
-        Please report to your supervisor for further instruction.
-      </p>
-      <p className={css.small}>
-        Compiling GPT-43
-        <br />
-        Compilation successful
-        <br />
-        Booting...
+        Find me at the nearest church. See you there
       </p>
     </div>
   );
